7d8d48dee5720acd09689a4a5565544c29210da7
srikanth_venagurthy <srikanth.venagurthy@altumindtech.com>
2024-09-09 11:02:19 +0530

    removed engagement module fn

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -13,7 +13,7 @@ import OurImpactByNumbers from '../Components/services-common/OurImpactByNumbers
 import LogoMarquee from "../Components/common-components/LogoMarquee"
 import { gradientStyle } from "../ReactFunctions"
 import Spectrum from "../Components/common-components/Spectrum"
-import UpdatedEngagementModel from "../Components/services-common/UpdatedEngagementModel"
+// import UpdatedEngagementModel from "../Components/services-common/UpdatedEngagementModel"
 
 const Services = () => {
   return (
@@ -66,7 +66,7 @@ const Services = () => {
         <WhyAltumind />
       </section>
       <OurPhilosophy />
-      <UpdatedEngagementModel />
+      {/* <UpdatedEngagementModel /> */}
       <section>
         <div className="text-center space-y-2 mb-20 font-raleway">
           <p className="text-sm md:text-base dark:text-white text-black dark:font-normal font-semibold">
